f977a40ab31593395b8f19ce9a4e2b743a2b114a
Reply-Noeud <breplyfabrice@gmail.com>
2022-03-28 20:20:13 +0200

    adding!

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const popUp = document.getElementsByClassName('portfolio__item-pop');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -11,3 +12,38 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     });
 });
+
+
+/*VIDEO GALLERY*/
+var videoPlayer = document.getElementById('videoPlayer');
+var myVideo = document.getElementById('myVideo');
+
+function stopVideo () {
+    videoPlayer.style.display = 'none';
+    myVideo.pause();
+}
+
+function playVideo (file) {
+    myVideo.src = file;
+    videoPlayer.style.display = 'block';
+    myVideo.autoplay = true;
+}
+
+
+
+
+/* IMAGE GALLERY */
+
+
+
+document.querySelectorAll('.portfolio__item img').forEach(image => {
+    image.onclick = () => {
+    document.querySelector('.popup-image').style.display = 'block';
+    document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }
+});
+
+
+document.querySelector('.popup-image span').onclick = () => {
+    document.querySelector('.popup-image').style.display = 'none';
+}
